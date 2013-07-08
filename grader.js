b44127ecfb45b6594e90faddcf6dfb9ce2689b1d
Edman Anjos <edmanjos@gmail.com>
2013-07-08 18:39:12 +0000

    Adds comments and reference for commander.js

--- grader.js
+++ grader.js
@@ -11,6 +11,10 @@
  *   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
  *   - http://maxogden.com/scraping-with-node.html
  *
+ * + commander.js
+ *   - https://github.com/visionmedia/commander.js
+ *   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
+ *
  * + JSON
  *   - http://en.wikipedia.org/wiki/JSON
  *   - https://developer.mozilla.org/en-US/docs/JSON
